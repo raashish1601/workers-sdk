be2a86ae52391d6b69a1e277d72ff92a776668a4
Rui Figueira <rfigueira@cloudflare.com>
2026-03-27 14:01:01 +0000

    BRAPI-1010: Add experimental headful browser rendering support to wrangler / vite plugin

--- packages/miniflare/src/plugins/browser-rendering/index.ts
+++ packages/miniflare/src/plugins/browser-rendering/index.ts
@@ -32,6 +32,7 @@ const BrowserRenderingSchema = z.object({
 	remoteProxyConnectionString: z
 		.custom<RemoteProxyConnectionString>()
 		.optional(),
+	headful: z.boolean().optional(),
 });
 
 export const BrowserRenderingOptionsSchema = z.object({
@@ -120,10 +121,12 @@ export const BROWSER_RENDERING_PLUGIN: Plugin<
 
 export async function launchBrowser({
 	browserVersion,
+	headful,
 	log,
 	tmpPath,
 }: {
 	browserVersion: string;
+	headful?: boolean;
 	log: Log;
 	tmpPath: string;
 }) {
@@ -202,9 +205,7 @@ export async function launchBrowser({
 		"--password-store=basic",
 		"--use-mock-keychain",
 		`--disable-features=${disabledFeatures.join(",")}`,
-		"--headless=new",
-		"--hide-scrollbars",
-		"--mute-audio",
+		...(headful ? [] : ["--headless=new", "--hide-scrollbars", "--mute-audio"]),
 		"--disable-extensions",
 		"about:blank",
 		"--remote-debugging-port=0",
